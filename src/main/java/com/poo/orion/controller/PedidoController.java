b4ece0a3b52900cc19aa67a7320cd00db461b618
MariaEDuarda005 <mariaesferreira05@gmail.com>
2025-11-10 21:10:29 -0300

    Controller pedido

--- src/main/java/com/poo/orion/controller/PedidoController.java
+++ src/main/java/com/poo/orion/controller/PedidoController.java
@@ -1,9 +1,40 @@
 package com.poo.orion.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.poo.orion.DTO.PedidoDTO;
+import com.poo.orion.Service.PedidoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pedidos")
 public class PedidoController {
+
+    private final PedidoService service;
+
+    public PedidoController(PedidoService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PedidoDTO>> getAllPedidos(){
+        return ResponseEntity.ok(service.getAllPedidos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PedidoDTO> getPedidoById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getPedidoById(id));
+    }
+
+    @PostMapping("/atualizar/{id}")
+    public ResponseEntity<PedidoDTO> atualizarPedido(@PathVariable Long id, String status){
+        return ResponseEntity.ok(service.updatePedidoStatus(id, status));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPedido(@PathVariable Long id) {
+        service.deletePedido(id);
+        return ResponseEntity.noContent().build();
+    }
 }
