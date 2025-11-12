74dded075b7849b9e15fc75a6d0d37fae518b9f3
CarolinaPinheiroSantos <pinheirocarol0506@gmail.com>
2025-11-12 20:14:34 -0300

    add front-end

---
+++ front/orion-fitness/src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/header';
+import AppRoutes from './router/appRoutes';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <main style={{ padding: "1rem 2rem" }}>
+        <AppRoutes />
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App
---
+++ front/orion-fitness/src/components/header.tsx
@@ -0,0 +1,22 @@
+import { Link, useLocation } from "react-router-dom";
+import Logo from "../assets/logo.png"
+import "../css/header.css";
+
+export default function Header() {
+  const { pathname } = useLocation();
+
+  return (
+    <header className="header">
+      <div className="infos">
+        <img src={Logo} alt="Logo Orion" />
+        <h1 className="logo">Orion Fitness</h1>
+      </div>
+      <nav className="nav">
+        <Link to="/" className={pathname === "/" ? "active" : ""}>Home</Link>
+        <Link to="/sobre" className={pathname === "/sobre" ? "active" : ""}>Sobre</Link>
+        <Link to="/produtos" className={pathname === "/produtos" ? "active" : ""}>Produtos</Link>
+        <Link to="/admin" className={pathname === "/admin" ? "active" : ""}>Admin</Link>
+      </nav>
+    </header>
+  );
+}
