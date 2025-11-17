286fdbccf15ae274ddd4a13953a85635b096c182
Ferreira Maria <fmr8ca@bosch.com>
2025-11-16 22:56:55 -0300

    a

--- back/orion/src/main/java/com/poo/orion/DTO/CarrinhoDTO.java
+++ back/orion/src/main/java/com/poo/orion/DTO/CarrinhoDTO.java
@@ -2,11 +2,14 @@ package com.poo.orion.DTO;
 
 import com.poo.orion.Model.Carrinho;
 
-public record CarrinhoDTO(Long produtoId, int quantidade) {
+import java.math.BigDecimal;
+
+public record CarrinhoDTO(Long produtoId, int quantidade, BigDecimal precoUnitario) {
     public static CarrinhoDTO from(Carrinho c) {
         return new CarrinhoDTO(
                 c.getProduto().getIdProduto(),
-                c.getQuantidade()
+                c.getQuantidade(),
+                c.getPrecoUnitario()
         );
     }
 }
